57cb0502275350c93147d59fd30a90b881b6e3c8
Joel <joel.balmer@gmail.com>
2020-01-20 14:08:04 +0000

    first working signal r communication :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PlanningPokerAngular';
+  message = 'No messages yet';
+
+  ngOnInit(): void {
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl('https://localhost:44394/notify')
+      .configureLogging(signalR.LogLevel.Debug)
+      .build();
+
+    connection.start().then(() => {
+      console.log('Connected!');
+    }).catch((err) => {
+      return console.error(err.toString());
+    });
+
+    connection.on('BroadcastMessage', (type: string, payload: string) => {
+      this.message = payload;
+    });
+  }
 }
