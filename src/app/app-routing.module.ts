7f0f3af5a5e946bf889d9ce543df4774e94abdf8
Joel <joel.balmer@gmail.com>
2020-01-24 15:21:10 +0000

    add welcome page to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'welcome' },
+  { path: 'welcome', component: WelcomePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
