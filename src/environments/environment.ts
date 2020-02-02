159c3f7808d714863bb75ddf9d02c8bb3a486976
joelAirMac <joel.balmer@gmail.com>
2020-02-02 18:31:23 +0000

    saving correct mac windows env api urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   apiUrl: {
-    windows: 'https://localhost:44394/api/',
-    mac: 'https://localhost:5001/api/',
+    windows: 'https://localhost:44394/api',
+    mac: 'https://localhost:5001/api',
   }
 };
 
