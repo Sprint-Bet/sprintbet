b46bfb5a768e19ae36017cba6f5ba480743308af
joelAirMac <joel.balmer@gmail.com>
2020-02-09 19:19:58 +0000

    added voting cards markup, styled vote locking

---
+++ src/app/components/voting-cards/voting-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voting-cards',
+  templateUrl: './voting-cards.component.html',
+  styleUrls: ['./voting-cards.component.scss']
+})
+export class VotingCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
