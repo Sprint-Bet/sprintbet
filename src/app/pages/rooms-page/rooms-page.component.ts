a530806b177aae59bb6ce2143346600ae1da4b6d
joelAirMac <joel.balmer@gmail.com>
2020-02-15 21:20:30 +0000

    correctly adding and combining!

--- src/app/pages/rooms-page/rooms-page.component.ts
+++ src/app/pages/rooms-page/rooms-page.component.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 import { NewVoter } from '@src/app/model/dtos/new-voter';
 import { NewVote } from '@src/app/model/dtos/new-vote';
 import { HubEvents } from '@src/app/model/enums/hubEvents.enum';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rooms-page',
@@ -17,8 +17,12 @@ export class RoomsPageComponent implements OnInit {
 
   initialVoters$ = this.voteService.setupVoter(this.name);
   newVoter$ = this.voteService.listenFor<NewVoter>(HubEvents.VoterAdded);
-  voters$ = combineLatest([this.initialVoters$, this.newVoter$]).pipe(
+  voters$ = combineLatest([
+    this.initialVoters$,
+    this.newVoter$.pipe(startWith(null)),
+  ]).pipe(
     map(([voters, newVoter]) => {
+      if (newVoter == null) return voters;
       return !voters[newVoter.id]
         ? voters[newVoter.id] = { name: newVoter.name, point: '' }
         : voters;
@@ -35,14 +39,6 @@ export class RoomsPageComponent implements OnInit {
     private route: ActivatedRoute,
   ) { }
 
-  ngOnInit() {
-    // combineLatest([this.initialVoters$, this.newVoter$]).pipe(
-    //   map(([voters, newVoter]) => {
-    //     return !voters[newVoter.id]
-    //       ? voters[newVoter.id] = { name: newVoter.name, point: '' }
-    //       : voters;
-    //   }),
-    // );
-  }
+  ngOnInit() { }
 
 }
