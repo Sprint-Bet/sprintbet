d12fe3e01383156552d3f628c86e0aee1eb88452
Joel-INTO <46967356+Joel-INTO@users.noreply.github.com>
2020-04-14 11:58:47 +0100

    have now converted to post rquest

--- src/app/model/enums/hubEvents.enum.ts
+++ src/app/model/enums/hubEvents.enum.ts
@@ -1,6 +1,4 @@
 export enum HubEvents {
-    // TODO: convert to API post request
-    // VoteUpdated = 'VoteUpdated',
     VoterAdded = 'VoterAdded',
     VoterLeft = 'VoterLeft',
     VotingUpdated = 'VotingUpdated',
