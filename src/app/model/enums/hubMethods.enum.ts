0661cef378b1fa3f16b6322958d5fabdd4a2ba4a
Joel-INTO <46967356+Joel-INTO@users.noreply.github.com>
2020-04-14 12:26:39 +0100

    updated hub events

--- src/app/model/enums/hubMethods.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum HubMethods {
-    // ATTEMPTING TO DO THIS VIA POST REQUEST NOW
-    // UpdateVote = 'UpdateVote',
-    SetupPlayer = 'SetupPlayer',
-    RemoveVoter = 'RemoveVoter',
-}
---
+++ src/app/services/hub-services/hubMethods.enum.ts
@@ -0,0 +1,4 @@
+export enum HubMethods {
+    // Consider the following via post too
+    RemoveVoter = 'RemoveVoter',
+}
