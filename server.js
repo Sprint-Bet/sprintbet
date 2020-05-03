e0f0ce2663a1daf146d03017203b92f4cca65e5a
Joel-INTO <46967356+Joel-INTO@users.noreply.github.com>
2020-05-03 15:39:55 +0100

    typo in server file!

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/PlanningPokerAngular'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/PlanningPokerAngularE/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/PlanningPokerAngular/index.html'));
 });
 
 // default Heroku PORT
