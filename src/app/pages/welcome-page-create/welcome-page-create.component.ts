1cfb5c146e10a98cc58634448b8b10a73dab6be7
Joel-INTO <46967356+Joel-INTO@users.noreply.github.com>
2020-05-17 15:22:47 +0100

    got create page working with correct flow

--- src/app/pages/welcome-page-create/welcome-page-create.component.ts
+++ src/app/pages/welcome-page-create/welcome-page-create.component.ts
@@ -16,23 +16,8 @@ import { RoleType } from '@src/app/enums/role-type.enum';
 export class WelcomePageCreateComponent implements OnInit {
   title = this.activatedRoute.snapshot.data.title;
 
-  private _isDealer = true;
-
-  get isDealer(): boolean {
-    return this._isDealer;
-  }
-
-  set isDealer(isDealer: boolean) {
-    this._isDealer = isDealer;
-
-    const group = this.registrationForm.get('group');
-    this.isDealer ? group.clearValidators() : group.setValidators(Validators.required);
-    group.updateValueAndValidity({ onlySelf: true });
-  }
-
   registrationForm = this.formBuilder.group({
     name: ['', Validators.required],
-    group: [''],
   });
 
   loading$ = this.store.pipe(select(loadingSelector));
@@ -45,35 +30,18 @@ export class WelcomePageCreateComponent implements OnInit {
 
   ngOnInit() {
     this.store.dispatch(welcomeComponentNavigatedAction());
-
-    const roomId = this.activatedRoute.snapshot.queryParamMap.get('id');
-    if (!!roomId) {
-      this.registrationForm.get('group').setValue(roomId);
-      this.isDealer = false;
-    }
-  }
-
-  createRoom() {
-    this.isDealer = true;
-    const registrationInfo = this.getRegistrationInfo(this.registrationForm);
-    this.store.dispatch(welcomePageCreateRoomClickedAction({ registrationInfo }));
   }
 
   onSubmit(form: FormGroup) {
-    if (this.isDealer) {
-      this.isDealer = false;
-      return;
-    }
-
     const registrationInfo = this.getRegistrationInfo(form);
-    this.store.dispatch(welcomePageJoinRoomClickedAction({ registrationInfo }));
+    this.store.dispatch(welcomePageCreateRoomClickedAction({ registrationInfo }));
   }
 
   getRegistrationInfo(form: FormGroup): NewVoter {
     return {
-      role: this.isDealer ? RoleType.DEALER : RoleType.PARTICIPANT,
-      name: form.get('name').value,
-      group: form.get('group').value,
+      ...form.value,
+      group: '',
+      role: RoleType.DEALER,
     };
   }
 
